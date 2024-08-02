b78d219db0db3305621cfaac02914144baabce89
Coenri van Rooyen <89804305+Coenri@users.noreply.github.com>
2024-08-02 16:59:19 +0200

    Many changes as well as new files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
+navMenu.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
